927742c4682d2ba0b986617dcc848017a0abea15
Or Tichon <or.tichon@gmail.com>
2024-08-30 20:09:31 +0300

    install and register swiper.js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
+
+// Register Swiper custom elements. We do this
+// before bootstrapping the Angular application
+// so that they're available before any part of
+// our application tries rendering them.
+registerSwiperElements();
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
