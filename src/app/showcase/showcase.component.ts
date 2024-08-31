43581630eb8919bfefd25af8fc1434713ec9b965
Or Tichon <or.tichon@gmail.com>
2024-08-31 05:08:26 +0300

    fixed showcase for phones

--- src/app/showcase/showcase.component.ts
+++ src/app/showcase/showcase.component.ts
@@ -14,9 +14,32 @@ import { NgForOf, NgOptimizedImage } from '@angular/common';
 })
 export class ShowcaseComponent {
 
-  pictureNumbers: number[] = [];
   @ViewChild('swiper', { static: true }) swiperElement: ElementRef;
 
+  pictureNumbers: number[] = [];
+
+  breakPoints = {
+    320: {
+      slidesPerView: 2.5, /* Show part of the second slide on very small screens */
+      centeredSlides: false
+    },
+    480: {
+      slidesPerView: 4, /* Show part of the third slide on small screens */
+      centeredSlides: false
+    },
+    768: {
+      slidesPerView: 4.5, /* Show part of the fourth slide on medium screens */
+      centeredSlides: false
+    },
+    1024: {
+      slidesPerView: 6, /* Show part of the fifth slide on larger screens */
+      centeredSlides: false
+    },
+    1280: {
+      slidesPerView: 6, /* Show 5 full slides on larger screens */
+      centeredSlides: false
+    }
+  }
 
   constructor() {
     this.pictureNumbers = Array.from({length: 21}, (v, k) => k + 1);
